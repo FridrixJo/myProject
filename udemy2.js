e3cb4101a8e0e417dd4143c9e5ee19aace13b568
machineglytkelly <pglutov@gmail.com>
2020-04-14 13:39:15 +0300

    first commit

---
+++ udemy2.js
@@ -0,0 +1,65 @@
+'use strict';
+alert(5);
+
+let LeftBorderWidth = 1 ;
+let second = 2 ;
+const pi = 3.14;
+
+let number = 5;
+let string = 'hey';
+let sym = Symbol();
+null;
+undefined;
+let obj = {};
+
+// console.log(something); // null
+
+/*let solution;
+console.log(solution); //undefined
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(person.name); //объект
+console.log(person["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'] //массив
+console.log(arr[0]);
+
+alert('How are you?');
+confirm('Are you here?');
+prompt('How old do you?');
+
+let answer = prompt('How old do you?');
+    if (answer >= 18 && answer <= 30) {
+        console.log('suck my dick')
+    } else if (answer < 18) {
+        console.log('do not touch my pines')
+    } else {
+        console.log('fuck off')
+    }
+
+let ques = prompt('you are cock?');
+    console.log(typeof(ques)); */
+
+console.log('arr' + ' - object');
+console.log(9 + 'arr');
+
+/*let ques = +prompt('you are cock?');
+    console.log(typeof(ques)); */
+
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+console.log(incr,decr);
+console.log(5231%23);
+console.log('2' == 2);
+console.log('2' === 2);
+
+let isChecked = true;
+    isFalse = false;
+console.log(isChecked || isFalse);
